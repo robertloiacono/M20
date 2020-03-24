ab0c5484d5c292d8b3a4f8ca13587fa5a6ddb1a6
robertloiacono <50601102+robertloiacono@users.noreply.github.com>
2020-03-24 00:00:57 -0700

    Add files via upload

--- Loiacono_305106844_Project_main.m
+++ Loiacono_305106844_Project_main.m
@@ -1,10 +1,16 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   Final Project MATLAB Script
 %   Author: Robert Loiacono
-%   Date:   3/13/20
+%   Date:   3/20/20
 %   UID: 305106844
 %   Code for Final Project
-%   This problem      
+%   The goal in this problem is to simulate the Magnetic Ising model that 
+%   consists of approximating the magnetic dipole moments in one of two 
+%   possible states: positve(+1) and negative(-1). By theoretically changing 
+%   a state at a particular site, you are able to calculate whether this 
+%   scenario is possible by examining the spins of neighboring sites. Over 
+%   one million iterations, the script will calculate and change the spins 
+%   of a random site and will output certain iterations in a plot and video format.      
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%  Clear Cache
@@ -12,7 +18,14 @@ clear all
 close all
 clc
 
+%%VIDEO CODE---------------------------------------------------
+% vidfile=VideoWriter('test3','MPEG-4');
+% vidfile.FrameRate=60;
+% vidfile.Quality=100;
+% open(vidfile);
+%------------------------------------------------------------------------
 %%MAIN SCRIPT
+%initialize values of grid size and iteration numbers
 J=1;
 kT=0.1;
 x_min=0;
@@ -20,7 +33,7 @@ x_max=150;
 y_min=0;
 y_max=100;
 h=1;
-currIter=1;
+currIter=0;
 maxIter=1000000;
 
 grid_type=input('Please enter grid-type: 1, 2 ,3: ');
@@ -28,37 +41,61 @@ while grid_type~=1 && grid_type~=2 && grid_type~=3
    fprintf('Error: Please enter a correct grid-type: 1, 2, 3\n');
    grid_type=input('Please enter grid-type: 1, 2 ,3: '); 
 end
-
+%set up initial field and plot its original state
 [dipole_field]=seed_initial(x_min,x_max,y_min,y_max,h,grid_type);
-%function 1
+plot_dipole(dipole_field,currIter,grid_type);
+
+maxJPts=((x_max-x_min)/h) +1;
+maxIPts=((y_max-y_min)/h) +1;
 while currIter<=maxIter
+    currIter=currIter+1;
+    %choose one site at random to change its spin configuration
+    j=floor(rand()*maxJPts)+1;
+    i=floor(rand()*maxIPts)+1;
+    %calculate the proposed spin
+    [delE,Prob]=ProbCalculation(dipole_field,i,j,J,kT);
+    randomNumber=rand;
     
-    [delE,Prob]=ProbCalculation(dipole_field,i,j,J,kT)
-    r=rand;
-    if delE<=0
-        new_field=update_field(dipole_field(i,j));
-    elseif delE>0 && r<=Prob
-        new_field=update_field(dipole_field(i,j));
+    if Prob==1
+        %spin is accepted
+        new_field=update_field(dipole_field,i,j);
+        dipole_field=new_field;
     else
-        new_field=dipole_field(i,j);
+        if randomNumber<=Prob
+            %spin is accepted
+            new_field=update_field(dipole_field,i,j);
+            dipole_field=new_field;
+%         else
+%             %spin is rejected
+%             new_field=dipole_field(i,j);
+        end
     end
-    dipole_field=new_field;
-    if currIter==1 || currIter==10 || currIter==100 || currIter==1000 || currIter==10000 || currIter==100000 || currIter==1000000
+    %dipole_field=new_field;
+    
+    if currIter==10 || currIter==100 || currIter==1000 || currIter==10000 || currIter==100000 || currIter==1000000
         plot_dipole(dipole_field,currIter,grid_type);
     end
-    currIter=currIter+1;
+%VIDEO CODE---------------------------------------------------
+%     if mod(currIter,1000)==0
+%         plot_dipole(dipole_field,currIter,grid_type);
+%         writeVideo(vidfile,getframe(gcf));
+%     end
+%-------------------------------------------------------------
 end
-
-
-%%Function 1 Initial Condition Function
+%VIDEO CODE---------------------------------------------------
+%close(vidfile)
+%-------------------------------------------------------------
+%%Function 1 Initial Condition Function 
+%initializes all the dipoles of every location in the matrix
 function [dipole_field]=seed_initial(x_min,x_max,y_min,y_max,h,grid_type)
-    maxJPts=(x_max-x_min)/h +1;
-    maxIPts=(y_max-y_min)/h+1;
-    %dipole_field=zeros(rows,cols);
+    maxJPts=((x_max-x_min)/h) +1;
+    maxIPts=((y_max-y_min)/h) +1;
+    dipole_field=zeros([maxIPts,maxJPts]);%blank dipole array
     if grid_type==1
-        for i=1:1:maxJPts
-            for j=1:1:maxIPts
-                if dipole_field(i,j)>=0.5
+        for i=1:1:maxIPts%iterate through columns first then rows
+            for j=1:1:maxJPts
+                r=rand;
+                if r>=0.5
                     dipole_field(i,j)=1;
                 else
                     dipole_field(i,j)=-1;
@@ -66,9 +103,9 @@ function [dipole_field]=seed_initial(x_min,x_max,y_min,y_max,h,grid_type)
             end
         end
     elseif grid_type==2
-        left_Half=0.5*maxJPts;
-        for i=1:1:maxJPts
-            for j=1:1:maxIPts
+        left_Half=0.5*maxJPts; %equalt to half the number of clumns
+        for i=1:1:maxIPts %iterate through columns first then rows
+            for j=1:1:maxJPts
                 if j<left_Half
                     dipole_field(i,j)=1;
                 else
@@ -77,28 +114,47 @@ function [dipole_field]=seed_initial(x_min,x_max,y_min,y_max,h,grid_type)
             end
         end
     else
-        %TODO
+        i_center=ceil((y_max-y_min)/2)+1;
+        j_center=ceil((x_max-x_min)/2)+1;
+        radius=i_center;
+        for i=1:1:maxIPts %iterate through columns first then rows
+            for j=1:1:maxJPts
+                if((i-i_center)^2 + (j-j_center)^2) <=radius^2 %anything inside this circle becomes positive
+                    dipole_field(i,j)=1;
+                else
+                    dipole_field(i,j)=-1;
+                end
+            end
+        end
     end
 end
-%pick a random point on field
-%flip its sign
-%calculate its prob now being the other sign using its neighbors
-%pick a random number and compare to probability
-%if rand>prob
-%reject spin
 
 %%Function 2- Energy and Probability Calculation Function
+%calculates the change and energy and probability of the random point
 function[delE,Prob]=ProbCalculation(dipole_field,i,j,J,kT)
-    maxJPts=(x_max-x_min)/h+1;
-    maxIPts=(y_max-y_min)/h+1;
+    fieldSize=size(dipole_field);
+    i_max=fieldSize(1);
+    j_max=fieldSize(2);
+    j_min=0;
+    i_min=0;
+    h=1;
+    maxJPts=(j_max-j_min)/h;
+    maxIPts=(i_max-i_min)/h;
     
+    %flip spin of random point
+    dipole_field(i,j)=dipole_field(i,j)*-1;
+%     temp_field=dipole_field;
+%     temp_field(i,j)=temp_field(i,j)*-1;
+%     dipole_field=temp_field;
+    
+    %calculate change in energy
     if j==1 %left side of matrix
         if i==1 %top left corner
             delE=-1*J*dipole_field(i,j)*(dipole_field(i,j+1)+dipole_field(i+1,j)+dipole_field(maxIPts,j)+dipole_field(i,maxJPts));
-        elseif i==maxYPts %bottom left corner
+        elseif i==maxIPts %bottom left corner
             delE=-1*J*dipole_field(i,j)*(dipole_field(i-1,j)+dipole_field(i,j+1)+dipole_field(1,1)+dipole_field(i,maxJPts));
         else %anywhere on the left wall of matrix not in corners
-            delE=-1*J*dipole_field(i,j)*(dipole_field(i-1,j)+dipole_field(i+1,j)+dipole_field(i,j+1)+dipole_field(i,maxJPts)));
+            delE=-1*J*dipole_field(i,j)*(dipole_field(i-1,j)+dipole_field(i+1,j)+dipole_field(i,j+1)+dipole_field(i,maxJPts));
         end
     elseif j==maxJPts %right side of matrix
         if i==1 %top right corner
@@ -115,27 +171,34 @@ function[delE,Prob]=ProbCalculation(dipole_field,i,j,J,kT)
     else %middle cell
         delE=-1*J*dipole_field(i,j)*(dipole_field(i+1,j)+dipole_field(i-1,j)+dipole_field(i,j+1)+dipole_field(i,j-1));
     end
+    
     %finding probability
     if delE<=0
         Prob=1;
     else
         Prob=exp(-delE/kT);
     end
-    
+    %flip spin back at random point and let update field function handle
+    %changing it if needed
+    dipole_field(i,j)=dipole_field(i,j)*-1;
 end 
 
 %%Update Field Function
-function [new_field]=update_field(dipole_field)
-%TODOOOOOOOOOOOOOOOOOOOOOOOOOOO
-
+%updates the dipole field if there needs to be a change in spin
+function [new_field]=update_field(dipole_field,i,j)
+%if proposed spin is accepted
+    new_field=dipole_field;
+    new_field(i,j)=new_field(i,j)*-1;
 end
 
 %%Plot Field Function 
+%plots the current iterations dipole field matrix
 function plot_dipole(dipole_field,currIter,grid_type)
+%yellow is positive spin and purple is negative; can be seen in colorbar
 figure()
 imagesc(dipole_field)
 colorbar
-title('Iteration Number'+num2str(currIter)+ 'For Grid Type'+num2str(grid_type),'FontSize',24);
+title(['Iteration Number ' num2str(currIter) ' For Grid Type ' num2str(grid_type)],'FontSize',20);
 %set(gcf,'Position',[0 0 150 100])
 hold on 
 grid on
